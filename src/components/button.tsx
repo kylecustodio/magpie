6f6cd5d55ada787cb6108d4dd78d17a087c5703f
kylecustodio <kkzycg@gmail.com>
2023-08-25 17:52:38 -0500

    Fix button click handler

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,14 @@
 export interface ButtonProps {
+  onClick?: () => void;
   children: React.ReactNode;
 }
 
 const Button = (props: ButtonProps) => {
   return (
-    <button className="text-black h-12 border-black border-2 p-2.5 bg-cyan-300 hover:bg-cyan-400 hover:shadow-[2px_2px_0px_rgba(0,0,0,1)] active:bg-cyan-300">
+    <button
+      onClick={props.onClick}
+      className="text-black h-12 border-black border-2 p-2.5 bg-cyan-300 hover:bg-cyan-400 hover:shadow-[2px_2px_0px_rgba(0,0,0,1)] active:bg-cyan-300"
+    >
       {props.children}
     </button>
   );
