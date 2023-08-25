6260ef1a85235277f935e2dc7f5c65e9a8862484
kylecustodio <kkzycg@gmail.com>
2023-08-25 17:52:29 -0500

    Route to signup on signup click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import Button from "@/components/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <main className="flex w-screen h-screen flex-col items-center p-24 bg-pink-200">
       <div className="flex flex-col gap-10 items-center">
         <h1 className="text-9xl text-black font-semibold">Magpie</h1>
         <div className="flex gap-4">
           <Button>Sign in</Button>
-          <Button>Sign up</Button>
+          <Button onClick={() => router.push("/signup")}>Sign up</Button>
         </div>
       </div>
     </main>
