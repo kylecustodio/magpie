2c42600f278f61904cdd3ffc3589296adfd05b09
kylecustodio <kkzycg@gmail.com>
2023-08-27 19:10:45 -0500

    Change background color to fuchsia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <main className="flex w-screen h-screen flex-col items-center p-24 bg-pink-200">
+    <main className="flex w-screen h-screen flex-col items-center p-24 bg-fuchsia-200">
       <div className="flex flex-col gap-10 items-center">
         <h1 className="text-9xl text-black font-semibold">Magpie</h1>
         <div className="flex gap-4">
