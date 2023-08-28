d27113f8e5afedc1c8e654fed9c840dc4449b9ac
kylecustodio <kkzycg@gmail.com>
2023-08-27 19:10:34 -0500

    Button enhancements

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,13 +1,14 @@
-export interface ButtonProps {
-  onClick?: () => void;
+import { ButtonHTMLAttributes } from "react";
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-const Button = (props: ButtonProps) => {
+const Button = (props: ButtonProps, children: React.ReactNode) => {
   return (
     <button
-      onClick={props.onClick}
       className="text-black h-12 border-black border-2 p-2.5 bg-cyan-300 hover:bg-cyan-400 hover:shadow-[2px_2px_0px_rgba(0,0,0,1)] active:bg-cyan-300"
+      {...props}
     >
       {props.children}
     </button>
