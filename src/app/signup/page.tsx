41deb9a17aa062256d564fbaf69f87ce8a0ef765
kylecustodio <kkzycg@gmail.com>
2023-09-04 18:39:44 -0500

    Add username selection to sign up

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -9,12 +9,13 @@ import { FormEvent, useState } from "react";
 const SignUp = () => {
   const router = useRouter();
 
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const onSubmit = async (event: FormEvent) => {
     event.preventDefault();
-    const { result, error } = await signUp(email, password);
+    const { result, error } = await signUp(username, email, password);
 
     if (error) {
       // handle error
@@ -31,12 +32,20 @@ const SignUp = () => {
         <h1 className="text-4xl text-black">Sign up</h1>
         <form className="flex flex-col gap-8" onSubmit={onSubmit}>
           <Input
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            name="username"
+            id="username"
+            placeholder="username"
+          ></Input>
+          <Input
             type="email"
             onChange={(e) => setEmail(e.target.value)}
             required
             name="email"
             id="email"
-            placeholder="example@example.com"
+            placeholder="email"
           />
           <Input
             type="password"
--- src/firebase/auth/signup.ts
+++ src/firebase/auth/signup.ts
@@ -1,13 +1,26 @@
 import app from "../config";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  updateProfile,
+} from "firebase/auth";
 
 const auth = getAuth(app);
 
-const signUp = async (email: string, password: string) => {
+const signUp = async (username: string, email: string, password: string) => {
   let result = null,
     error = null;
   try {
-    result = await createUserWithEmailAndPassword(auth, email, password);
+    const createResult = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const emailResult = await sendEmailVerification(createResult.user);
+    const updateResult = await updateProfile(createResult.user, {
+      displayName: username,
+    });
   } catch (e) {
     error = e;
   }
