f85c2bce4127f1e0754fd0e9f4c8c956747f1f46
kylecustodio <kkzycg@gmail.com>
2023-09-04 19:26:38 -0500

    Fill out home page
    - No actual interactivity

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,21 +1,53 @@
 "use client";
 
+import Button from "@/components/button";
+import TextArea from "@/components/text-area";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 const Home = () => {
-  const auth = useAuthContext();
+  const { user } = useAuthContext();
   const router = useRouter();
 
+  const [postMessage, setPostMessage] = useState<string>("");
+
+  const createPost = (event: FormEvent) => {
+    event.preventDefault();
+    // TODO: Create the post
+    console.log(postMessage);
+  };
+
   useEffect(() => {
-    if (auth?.user === null) {
+    if (user === null) {
       router.push("/");
     }
-  }, [auth]);
+  }, [user]);
 
   return (
-    <div className="w-screen h-screen bg-white text-black">Home workds</div>
+    <div className="w-screen h-screen bg-white text-black">
+      <div className="flex flex-col w-1/2 mx-auto items-center py-16 gap-8">
+        {user?.emailVerified ? (
+          <form className="flex flex-col gap-4" onSubmit={createPost}>
+            <TextArea
+              name="newPost"
+              id="newPost"
+              onChange={(e) => setPostMessage(e.target.value)}
+              required
+            ></TextArea>
+            <Button type="submit">Post</Button>
+          </form>
+        ) : (
+          <div>Verify your email to post.</div>
+        )}
+        <div className="flex flex-col grow gap-4">
+          <div>sample post</div>
+          <div>sample post</div>
+          <div>sample post</div>
+          <div>sample post</div>
+        </div>
+      </div>
+    </div>
   );
 };
 
